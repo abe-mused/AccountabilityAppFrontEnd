546685481f623ead0933bf5a9dde37485a8504b8
Anabelle Ndrejaj <gw0291@wayne.edu>
2022-09-22 11:43:58 -0400

    basic second page Testing push request, having issues with VScode github integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+//TESTING PUSH
+
 
 void main() {
   runApp(const MyApp());
@@ -17,14 +21,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
+      home: const HomePage(title: 'HomePage'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Linear Accountability App'),
         ),
-        body: const Center(
-          child: Text('Yo, this is the Linear Accountability App'),
+        body: Center(
+          child: TextButton(
+          onPressed:() {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const CommunityPage(title: 'CommunityPage');
+          }));
+          },
+          child: const Text('Community Page'),
+          ),
         ),
-      ),
+      );
+  }
+}
+
+class CommunityPage extends StatelessWidget{
+  const CommunityPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        ), 
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+            child: const Text('Go Back to Home Page'),
+          ),
+        ),
     );
   }
+
 }
