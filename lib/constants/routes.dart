3c836578fa8881dbd337752fc1f87011bae18b7e
Abe Mused <ebrahammused@gmail.com>
2022-10-24 12:29:44 -0400

    Refactor routes into a constant

---
+++ lib/constants/routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:linear/auth_pages/sign_in.dart';
+import 'package:linear/auth_pages/sign_up.dart';
+import 'package:linear/pages/community_page/community_page.dart';
+import 'package:linear/pages/create_community_page/create_community_page.dart';
+import 'package:linear/pages/home_page/home_page.dart';
+import 'package:linear/pages/profile_page/profile_page.dart';
+import 'package:linear/pages/search_page/search_page.dart';
+
+Map<String, Widget Function(BuildContext context)> linearRoutes = {
+  '/home': (context) => const HomePage(),
+  '/login': (context) => const LoginPage(),
+  '/signup': (context) => const SignUpPage(),
+  '/search': (context) => const SearchPage(),
+  '/profile': (context) => const ProfilePage(),
+  '/community': (context) => const CommunityPage(),
+  '/createCommunity': (context) => const CreateCommunityPage(),
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:linear/auth_pages/sign_in.dart';
 import 'package:linear/auth_pages/sign_up.dart';
+import 'package:linear/constants/routes.dart';
 import 'package:linear/pages/home_page/home_page.dart';
 import 'package:linear/pages/profile_page/profile_page.dart';
 import 'package:linear/pages/search_page/search_page.dart';
 import 'package:linear/pages/community_page/community_page.dart';
-import 'package:linear/pages/search_page/create_community_page.dart';
 import 'package:linear/util/cognito/user.dart';
 import 'package:linear/util/cognito/user_preferences.dart';
 import 'package:linear/util/cognito/user_provider.dart';
@@ -38,37 +38,30 @@ class LinearApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => userProvider),
       ],
       child: MaterialApp(
-          title: 'Linear',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          home: FutureBuilder(
-              future: getUserData(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return const CircularProgressIndicator();
-                  default:
-                    if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else if (isUserDataLoaded(snapshot)) {
-                      return const LoginPage();
-                    }
-                    return const HomePage();
-                }
-              }),
-          routes: {
-            '/home': (context) => const HomePage(),
-            '/login': (context) => const LoginPage(),
-            '/signup': (context) => const SignUpPage(),
-            '/search': (context) => const SearchPage(),
-            '/profile': (context) => const ProfilePage(),
-            '/community': (context) => const CommunityPage(),
-            '/createCommunity': (context) => const CreateCommunityPage(),
-          }),
+        title: 'Linear',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: FutureBuilder(
+            future: getUserData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return const CircularProgressIndicator();
+                default:
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (isUserDataLoaded(snapshot)) {
+                    return const LoginPage();
+                  }
+                  return const HomePage();
+              }
+            }),
+        routes: linearRoutes,
+      ),
     );
   }
 
