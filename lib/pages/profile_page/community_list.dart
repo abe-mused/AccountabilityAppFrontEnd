8adfb035170ea0abc66521a375d2525cb4f9f716
tanvirahmed725 <91294146+tanvirahmed725@users.noreply.github.com>
2022-10-29 01:53:55 -0400

    Redesign Profile page UI and add attribute to show if user created community

--- lib/pages/profile_page/community_list.dart
+++ lib/pages/profile_page/community_list.dart
@@ -4,10 +4,13 @@ import 'package:linear/pages/community_page/community_page.dart';
 
 class CommunityListWidget extends StatelessWidget {
   const CommunityListWidget(
-      {super.key, required this.user, required this.communityLength, required this.token});
+      {super.key,
+      required this.user,
+      required this.communityLength,
+      required this.token});
 
   final User user;
-  final int ?communityLength;
+  final int? communityLength;
   final String token;
 
   @override
@@ -25,26 +28,36 @@ class CommunityListWidget extends StatelessWidget {
               margin: const EdgeInsets.only(top: 10.0),
               child: Padding(
                 padding: const EdgeInsets.all(5.0),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     TextButton(
-                      onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CommunityPage(
-                            communityName: user.communities![index][0]['communityName'],
-                            token: token,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CommunityPage(
+                                communityName: user.communities![index][0]
+                                    ['communityName'],
+                                token: token,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "c/${user.communities![index][0]['communityName']}",
+                          style: const TextStyle(
+                            fontFamily: 'MonteSerrat',
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w800,
                           ),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "c/${user.communities![index][0]['communityName']}",
-                      style: const TextStyle(
-                          fontFamily: 'MonteSerrat', fontSize: 16),
-                      textAlign: TextAlign.left,
-                    )),
+                        )),
+                    if (user.username == user.communities![index][1]['creator'])
+                      const Icon(
+                        Icons.admin_panel_settings,
+                        color: Colors.blue,
+                        size: 30.0,
+                      ),
                   ],
                 ),
               ),
